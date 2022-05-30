4d370cfa755b07b2b910f53b98c8d6af85b0dd86
Cr7stian8 <cristianotropeiro@email.com>
2022-05-30 16:12:22 -0300

    Adicionando camada repository

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package com.generation.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.blogpessoal.model.Postagem;
+
+// A camada repository é responsável por consultar o banco de dados
+// criando um repositório de queries dentro da tabela de postgens (tb_postagem)
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+}
