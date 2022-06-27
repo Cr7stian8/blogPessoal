404a75d3c8fe7e505b83ca5a8d62f188f96d3da1
Cr7stian8 <cristianotropeiro@gmail.com>
2022-06-27 13:50:21 -0300

    Adicionando componente home

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,12 +1,12 @@
+import React from 'react';
 import './App.css';
+import Home from './pages/Home/Home';
 
 let nome = "Cristiano"
 
 function App() {
   return (
-    <>
-    <h1>ola {nome}</h1>
-    </>
+    <Home />
   );
 }
 
---
+++ blog-pessoal/src/pages/Home/Home.tsx
@@ -0,0 +1,19 @@
+// typerscript => javascript + tipagemfixa
+
+//rfce cria a estrutura de um componente
+import React from 'react'
+import './Home.css'
+
+//Grid tem 12 colunas
+function Home() {
+  return (
+    <>
+        <h1>hello</h1>
+        <h2>world</h2>
+    </>
+  )
+}
+
+export default Home
+
+// Sempre é necessário exportar a função
