83b883db529002a96722d05396a40a5b70f93077
Cr7stian8 <cristianotropeiro@gmail.com>
2022-06-28 11:38:21 -0300

    Adição página Home

--- blog-pessoal/src/paginas/Home/Home.tsx
+++ blog-pessoal/src/paginas/Home/Home.tsx
@@ -1,19 +1,50 @@
-// typerscript => javascript + tipagemfixa
-//rfce cria a estrutura de um componente
+import React from 'react';
+import { Typography, Box, Grid, Button } from '@material-ui/core';
+import './Home.css';
 
-import './Home.css'
-
-
-
-//Grid tem 12 colunas
 function Home() {
   return (
     <>
-      <h1>Conteúdo da HOME</h1>
+    {/* Display da página */}
+      <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+
+        {/* Primeiro Grid */}
+
+        <Grid alignItems="center" item xs={6} >
+          <Box paddingX={20} >
+
+            <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vinde!
+            </Typography>
+
+            <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!
+            </Typography>
+
+          </Box>
+
+            <Box display="flex" justifyContent="center">
+              <Box marginRight={1}>
+            </Box>
+
+            <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>
+              Ver Postagens
+            </Button>
+
+          </Box>
+        </Grid>
+        
+        { /* Segundo Grid */ }
+
+        <Grid item xs={6}>
+          <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+        </Grid>
+
+        <Grid xs={12} style={{ backgroundColor: "white" }}>
+        </Grid>
+
+      </Grid>
+
     </>
-  )
+  );
 }
 
-export default Home
-
-// Sempre é necessário exportar a função
+export default Home;
