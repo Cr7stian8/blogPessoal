e9d5af9c8f2e150237e55bcb46dae1e7261cea42
Cr7stian8 <cristianotropeiro@gmail.com>
2022-06-29 19:56:00 -0300

    Adição alterações na Home

--- blog-pessoal/src/paginas/Home/Home.tsx
+++ blog-pessoal/src/paginas/Home/Home.tsx
@@ -5,40 +5,62 @@ import './Home.css';
 function Home() {
   return (
     <>
-    {/* Display da página */}
-      <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+      {/* Display da página */}
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        className='caixa'
+        >
 
         {/* Primeiro Grid */}
 
         <Grid alignItems="center" item xs={6} >
           <Box paddingX={20} >
 
-            <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vinde!
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              className='titulo'
+            >Seja bem vindo(a)!
             </Typography>
 
-            <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              className='titulo'
+            >expresse aqui os seus pensamentos e opiniões!
             </Typography>
 
           </Box>
 
-            <Box display="flex" justifyContent="center">
-              <Box marginRight={1}>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}>
             </Box>
 
-            <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>
-              Ver Postagens
+            <Button
+              variant="outlined"
+              className='botao'
+            >Ver Postagens
             </Button>
 
           </Box>
         </Grid>
-        
-        { /* Segundo Grid */ }
+
+        { /* Segundo Grid */}
 
         <Grid item xs={6}>
           <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
         </Grid>
 
-        <Grid xs={12} style={{ backgroundColor: "white" }}>
+        <Grid xs={12} className='postagens'>
         </Grid>
 
       </Grid>
