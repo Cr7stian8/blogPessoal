ff482c4210e676563026b18c80b451b3d7d5a2db
Cr7stian8 <cristianotropeiro@gmail.com>
2022-07-06 15:26:59 -0300

    Fatorando navbar

--- frontend/src/componentes/estaticos/navbar/Navbar.tsx
+++ frontend/src/componentes/estaticos/navbar/Navbar.tsx
@@ -1,44 +1,58 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css'
+import useLocalStorage from 'react-use-localstorage';
 
 function Navbar() {
+    const [token, setToken] = useLocalStorage('token')
+    let navigate = useNavigate()
+
+    function sair() {
+        alert('Usuario deslogado')
+        navigate('/login')
+        setToken('')
+    }
     return (
         <>
             <AppBar position="static">
                 <Toolbar variant="dense" className='container'>
-                    <Box className='cursor' >
+                    <Box className='cursor tdn'>
                         <Typography variant="h5" color="inherit">
                             Cr7's, O blog.
                         </Typography>
                     </Box>
 
                     <Box display="flex">
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                home
-                            </Typography>
-                        </Box>
-
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
-                        </Box>
-
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
-
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
-                            </Typography>
-                        </Box>
-                        <Link to='/login' className='text-decoration-none'>
+                        <Link to='/home' className='tdn'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    home
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to='/posts' className='tdn'>
+                            <Box mx={1} className='cursor '>
+                                <Typography variant="h6" color="inherit">
+                                    postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to='/temas' className='tdn'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    temas
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to='/formularioTema' className='tdn'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    cadastrar tema
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to='/login' className='text-decorator-none' >
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
                                     sair
