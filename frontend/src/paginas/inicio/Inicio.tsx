408f37a8a499ba1191e0a9317d508c35b8cfae0a
Cr7stian8 <cristianotropeiro@gmail.com>
2022-07-26 12:33:15 -0300

    Criando componente da tela inicial

---
+++ frontend/src/paginas/inicio/Inicio.tsx
@@ -0,0 +1,144 @@
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import ModalPostagem from '../../componentes/postagens/modalPostagem/ModalPostagem';
+import { addToken } from '../../store/tokens/action';
+import { TokenState } from '../../store/tokens/tokenReducer';
+import '../inicio/Inicio.css';
+
+function Inicio() {
+    let rota = useLocation();
+
+    const token = useSelector<TokenState, TokenState['token']>(
+        (state) => state.token
+    )
+
+    const dispatch = useDispatch()
+
+    let navigate = useNavigate();
+
+    function goLogout() {
+        dispatch(addToken(''))
+        toast.info('usuario deslogado', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: 'colored',
+            progress: undefined
+        })
+        navigate('/login')
+    }
+
+    useEffect(() => {
+        if (token === "") {
+          toast.error('Você não está logado', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: 'colored',
+            progress: undefined
+          })
+          navigate("/login")
+        }
+        // eslint-disable-next-line
+      }, [token])
+
+
+    //Renderização condicional
+
+    var inicioComponent
+    if (token !== "" && rota.pathname === "/home") {
+        inicioComponent =
+
+            <AppBar position="static">
+                <Toolbar
+                    variant="dense"
+                    className='nav'>
+                    <Box
+                        justifyContent="start"
+                        className='wrap'>
+                        <Link
+                            to="/posts"
+                            className="tdn">
+                            <Box
+                                mx={1}
+                                className='cursor'>
+                                <Typography
+                                    className='texto-inicio a1'
+                                    color="inherit">
+                                    postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link
+                            to="/temas"
+                            className="tdn">
+                            <Box
+                                mx={1}
+                                className='cursor'>
+                                <Typography
+                                    className='texto-inicio a2'
+                                    color="inherit">
+                                    temas
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link
+                            to="/formularioTema"
+                            className="tdn">
+                            <Box
+                                mx={1}
+                                className='cursor'>
+                                <Typography
+                                    className='texto-inicio a3'>
+                                    Novo tema
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Box
+                            mx={1}
+                            className='cursor a4'>
+                            <Box
+                                display="flex"
+                                justifyContent="center">
+                                <Box
+                                    marginRight={1}
+                                    className='texto-inicio'>
+                                    <ModalPostagem />
+                                </Box>
+                            </Box>
+                        </Box>
+
+                        <Box
+                            mx={1}
+                            className='cursor'
+                            onClick={goLogout}>
+                            <Typography
+                                className='texto-inicio sair-inicio a5'>
+                                sair
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+    }
+
+    return (
+        <>
+            {inicioComponent}
+        </>
+    )
+}
+
+export default Inicio
